a2502a187d47457368e270cddfdf165a5cdf67c6
EspositoLucas <espositolucas2002@gmail.com>
2022-05-05 22:34:48 -0300

    Agrego base para test de validacion

---
+++ PrimeraEntrega/Implementacion/src/test/java/junit/TestValidadorContraseña.java
@@ -0,0 +1,70 @@
+package junit;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import validador.ResultadoDeValidacion;
+import validador.ValidadorContrasenia;
+
+
+public class TestValidadorContraseña {
+
+//    @Before
+//    public void setup(){
+//        ValidadorContrasenia contrasenia ;
+//    }
+    @Test
+
+    public void testBoludo() {
+        Assert.assertEquals(1,1);
+    }
+    //Tests para corroborar si una contrasenia pertenece a la lista de las 100000 peores contrasenias
+
+//    public void contrasenia_esta_dentro_del_top_10000_peores_contrasenias() {
+//        Assert.assertTrue
+//    }
+//    public void contrasenia_no_esta_dentro_del_top_10000_peores_contrasenias() {
+//        Assert.assertTrue(esValido: true);
+//    }
+//
+//    //Tests para corroborar si una contrasenia que no pertenece a la lista de las 100000 peores contrasenias cumple con las condiciones para ser segura
+//
+//    public void contrasenia_contiene_cantidad_minima_de_caracteres() {
+//        Assert.assertTrue(esValido: true);
+//    }
+//    public void contrasenia_no_contiene_cantidad_minima_de_caracteres() {
+//        Assert.assertEquals(1,1);
+//    }
+//
+//    public void contrasenia_contiene_cantidad_minima_de_letras_minusculas() {
+//        Assert.assertEquals(1,1);
+//    }
+//
+//    public void contrasenia_no_contiene_cantidad_minima_de_letras_minusculas() {
+//        Assert.assertEquals(1,1);
+//    }
+//
+//    public void contrasenia_contiene_cantidad_minima_de_letras_mayusculas() {
+//        Assert.assertEquals(1,1);
+//    }
+//
+//    public void contrasenia_no_contiene_cantidad_minima_de_letras_mayusculas() {
+//        Assert.assertEquals(1,1);
+//    }
+//
+//    public void contrasenia_contiene_cantidad_minima_de_digitos() {
+//        Assert.assertEquals(1,1);
+//    }
+//    public void contrasenia_no_contiene_cantidad_minima_de_digitos() {
+//        Assert.assertEquals(1,1);
+//    }
+//
+//    public void contrasenia_contiene_cantidad_minima_de_caracteres_especiales() {
+//        Assert.assertEquals(1,1);
+//    }
+//    public void contrasenia_no_contiene_cantidad_minima_de_caracteres_especiales() {
+//        Assert.assertEquals(1,1);
+//    }
+
+
+
+}
