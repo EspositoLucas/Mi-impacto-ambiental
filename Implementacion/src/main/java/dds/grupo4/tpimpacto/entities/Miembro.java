2d700c0b6b4e267f6909e7a0144e71fc2ddf5486
echisaidman <esaidman@frba.utn.edu.ar>
2022-06-05 16:19:44 -0300

    Arreglo estructura Persona/Miembro/Sector Una Persona tiene muchos Miembros, uno por cada Sector donde trabaja (el Miembro es la conexion muchos a muchos entre Persona y Sector) Cada Miembro tiene un Usuario (asi que la Persona tiene un Usuario diferente para cada Miembro suyo)

--- Implementacion/src/main/java/dds/grupo4/tpimpacto/entities/Miembro.java
+++ Implementacion/src/main/java/dds/grupo4/tpimpacto/entities/Miembro.java
@@ -1,73 +1,42 @@
 package dds.grupo4.tpimpacto.entities;
 
-import dds.grupo4.tpimpacto.enums.TipoDocumento;
-
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Entity(name = "Miembro")
 @Table(name = "miembros")
 public class Miembro extends BaseEntity {
 
-    private String nombre;
-    private String apellido;
-    private TipoDocumento tipoDocumento;
-    private Integer numeroDocumento;
-    private List<Sector> sectores = new ArrayList<>();
+    private Persona persona;
+    private Usuario usuario;
+    private Sector sector;
     private List<Trayecto> trayectos = new ArrayList<>();
 
     // Hibernate
     protected Miembro() {
     }
 
-    public Miembro(String nombre, String apellido, TipoDocumento tipoDocumento, Integer numeroDocumento) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.tipoDocumento = tipoDocumento;
-        this.numeroDocumento = numeroDocumento;
+    public Miembro(Persona persona, Sector sector) {
+        this.persona = persona;
+        this.sector = sector;
     }
 
-    public String getNombre() {
-        return nombre;
+    public Persona getPersona() {
+        return persona;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setPersona(Persona persona) {
+        this.persona = persona;
     }
 
-    public String getApellido() {
-        return apellido;
+    public Sector getSector() {
+        return sector;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public TipoDocumento getTipoDocumento() {
-        return tipoDocumento;
-    }
-
-    public void setTipoDocumento(TipoDocumento tipoDocumento) {
-        this.tipoDocumento = tipoDocumento;
-    }
-
-    public Integer getNumeroDocumento() {
-        return numeroDocumento;
-    }
-
-    public void setNumeroDocumento(Integer numeroDocumento) {
-        this.numeroDocumento = numeroDocumento;
-    }
-
-    public List<Sector> getSectores() {
-        return sectores;
-    }
-
-    public void setSectores(List<Sector> sectores) {
-        this.sectores = sectores;
+    public void setSector(Sector sector) {
+        this.sector = sector;
     }
 
     public List<Trayecto> getTrayectos() {
@@ -78,18 +47,13 @@ public class Miembro extends BaseEntity {
         this.trayectos = trayectos;
     }
 
-
     // Metodo para agregar trayecto
     public void agregarTrayecto(Trayecto trayecto) {
         this.trayectos.add(trayecto);
     }
 
-    // Metodo para obtener las organizaciones
-    public List<Organizacion> getOrganizaciones() {
-        return this.sectores.stream()
-                .map(s -> s.getOrganizacion())
-                .distinct()
-                .collect(Collectors.toList());
+    public Organizacion getOrganizacion() {
+        return this.sector.getOrganizacion();
     }
 
     // calculo para la huella de carbono
