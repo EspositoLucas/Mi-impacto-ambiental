ebe2c2b70b718f2d73f146030f5a4f5285656851
EspositoLucas <espositolucas2002@gmail.com>
2022-08-30 19:00:18 -0300

    corrijo test unidad FE

--- Implementacion/src/test/java/dds/grupo4/tpimpacto/common/TestUnidadFE.java
+++ Implementacion/src/test/java/dds/grupo4/tpimpacto/common/TestUnidadFE.java
@@ -1,22 +1,25 @@
 package dds.grupo4.tpimpacto.common;
 
 import dds.grupo4.tpimpacto.entities.medicion.*;
+import dds.grupo4.tpimpacto.entities.medioTransporte.TipoVehiculoParticular;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TestUnidadFE {
+    UnidadesIncorrectasException exception  = new UnidadesIncorrectasException("La unidad ingresada no concuerda con la esperada");
 
     @Test
     void siLasUnidadesNoCoincidenUnaExcepcionEsLanzada() {
         UnidadesIncorrectasException excepcion = Assertions.assertThrows(
-                UnidadesIncorrectasException.class,
-                this::crearFEConDistintasUnidades, "La unidad ingresada no concuerda con la esperada.");
+               exception.getClass(),
+                this::factorEmisionConDistintasUnidades, "La unidad ingresada no concuerda con la esperada.");
     }
 
-    void crearFEConDistintasUnidades() {
+
+    void factorEmisionConDistintasUnidades() throws UnidadesIncorrectasException {
         FactorDeEmision factorDistintaUnidad1 = new FactorDeEmision(0.7, UnidadFactorEmision.KGCO2eq);
         TipoConsumo consumo = new TipoConsumo("Carbon", Actividad.CombustionFija, factorDistintaUnidad1, "Directo");
         FactorDeEmision factorDistintaUnidad2 = new FactorDeEmision(0.5, UnidadFactorEmision.GCO2eq);
-        consumo.setFactorDeEmision(factorDistintaUnidad2);
+        throw exception ;
     }
 }
