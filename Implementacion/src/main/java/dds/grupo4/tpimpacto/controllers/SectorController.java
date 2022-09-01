4c6a8eae9bf7c31820f938440fa307c4f2516ce6
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-08-31 22:32:11 -0300

    Simplifico las Responses que solo devuelven una lista de resultados

--- Implementacion/src/main/java/dds/grupo4/tpimpacto/controllers/SectorController.java
+++ Implementacion/src/main/java/dds/grupo4/tpimpacto/controllers/SectorController.java
@@ -1,8 +1,9 @@
 package dds.grupo4.tpimpacto.controllers;
 
 import dds.grupo4.tpimpacto.dtos.CrearSectorRequest;
-import dds.grupo4.tpimpacto.dtos.ListarSectoresResponse;
+import dds.grupo4.tpimpacto.dtos.SectorDto;
 import dds.grupo4.tpimpacto.dtos.base.BaseResponse;
+import dds.grupo4.tpimpacto.dtos.base.ResponseWithResults;
 import dds.grupo4.tpimpacto.services.SectorService;
 import dds.grupo4.tpimpacto.utils.ResponseEntityUtils;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,7 @@ public class SectorController {
     }
 
     @GetMapping
-    public ResponseEntity<ListarSectoresResponse> listarSectores() {
+    public ResponseEntity<ResponseWithResults<SectorDto>> listarSectores() {
         return ResponseEntityUtils.toResponseEntity(sectorService.listarSectores());
     }
 
