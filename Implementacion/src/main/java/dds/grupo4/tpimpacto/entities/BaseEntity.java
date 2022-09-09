ca802c2fd760dc051900070878a89c50ab77fad3
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-09-09 02:42:18 -0300

    Vuelvo a meterle MappedSuperclass al BaseEntity Se lo habia sacado para lo del Soft-Delete, pero trae problemas

--- Implementacion/src/main/java/dds/grupo4/tpimpacto/entities/BaseEntity.java
+++ Implementacion/src/main/java/dds/grupo4/tpimpacto/entities/BaseEntity.java
@@ -3,11 +3,13 @@ package dds.grupo4.tpimpacto.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.util.Objects;
 
-@Entity(name = "BaseEntity")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@MappedSuperclass
 @Getter
 @Setter
 public abstract class BaseEntity {
