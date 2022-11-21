cb2a281410ad0cc9a47f7d9900eef313037b9275
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-11-21 01:06:37 -0300

    Fix menor

--- API/src/main/java/dds/grupo4/tpimpacto/DbInitializer.java
+++ API/src/main/java/dds/grupo4/tpimpacto/DbInitializer.java
@@ -43,5 +43,7 @@ public class DbInitializer implements ApplicationRunner {
         tipoConsumoService.seedData();
         organizacionService.seedData();
         personaService.seedData();
+
+        log.info("DbInitializer finalizado");
     }
 }
