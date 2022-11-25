ed229032a4113f4e4ee3d2d2c3edbf2053d35baf
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-11-25 00:57:30 -0300

    Logeo el environment en uso para testear cual agarra

--- Angular/src/main.ts
+++ Angular/src/main.ts
@@ -4,6 +4,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Logea el environment en uso para testear que agarre
+// el local o produccion correctamente
+console.log(environment);
+
 if (environment.production) {
   enableProdMode();
 }
