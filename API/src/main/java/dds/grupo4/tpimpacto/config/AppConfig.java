7fc7392e22ad343f3c3fc56cc2790eb72f42ae1f
Ezequiel Saidman <esaidman@frba.utn.edu.ar>
2022-12-05 01:55:34 -0300

    Agrego property para tener la URL de la API

--- API/src/main/java/dds/grupo4/tpimpacto/config/AppConfig.java
+++ API/src/main/java/dds/grupo4/tpimpacto/config/AppConfig.java
@@ -1,32 +1,14 @@
 package dds.grupo4.tpimpacto.config;
 
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
-import org.springframework.context.annotation.Bean;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurer;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import java.util.concurrent.Executor;
 
 @Configuration
-@EnableAsync
-public class AppConfig implements AsyncConfigurer {
+@Getter
+public class AppConfig {
 
-    @Override
-    @Bean
-    public Executor getAsyncExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(2);
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("DDSAsync-");
-        executor.initialize();
-        return executor;
-    }
+    @Value("${server.url}")
+    private String apiUrl;
 
-    @Override
-    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
-        return new CustomAsyncExceptionHandler();
-    }
 }
